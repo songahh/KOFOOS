4e6296fe9c0db60878862af375d097b34590c051
현민수 <gusalstn91@ajou.ac.kr>
2024-01-24 14:56:59 +0900

    chore: [develop] @test 어노테이션 주석

--- server/src/test/java/com/kofoos/api/ApiApplicationTests.java
+++ server/src/test/java/com/kofoos/api/ApiApplicationTests.java
@@ -3,7 +3,7 @@ package com.kofoos.api;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+// @SpringBootTest
 class ApiApplicationTests {
 
 	@Test
