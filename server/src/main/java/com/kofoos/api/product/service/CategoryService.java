4594bc78d41e13f59b1545bb01ab0f3042b8d9ac
현민수 <gusalstn91@ajou.ac.kr>
2024-02-02 10:55:33 +0900

    feat: [feature/productApi] 상품 api

---
+++ server/src/main/java/com/kofoos/api/product/service/CategoryService.java
@@ -0,0 +1,28 @@
+package com.kofoos.api.product.service;
+
+import com.kofoos.api.product.repository.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class CategoryService {
+
+    private final CategoryRepository categoryRepository;
+
+    public List<String> findCat2(String cat1){
+        return categoryRepository.findCat2(cat1);
+    }
+
+    public List<String> findCat3(String cat1,String cat2){
+        return categoryRepository.findCat3(cat1,cat2);
+    }
+
+    public List<String> ranking(){
+        return categoryRepository.ranking();
+    }
+
+
+}
