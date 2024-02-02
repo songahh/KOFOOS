963abe9086a54469381b9512a60ff4d3aff7a04d
권정훈 <dhtmxk8134@naver.com>
2024-02-02 16:09:59 +0900

    chore: [develop-front] 깃 충돌

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'KOFO',
+      title: 'KOF',
       initialBinding: BindingsBuilder(() {
         Get.put(RootController());
       }),
