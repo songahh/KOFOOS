23f5f59fffc702efff72e70648509611f5dd1b37
권정훈 <dhtmxk8134@naver.com>
2024-02-02 16:11:20 +0900

    chore: [develop-front] 제목 수정

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'KOFO',
+      title: 'KOFOOS',
       initialBinding: BindingsBuilder(() {
         Get.put(RootController());
       }),
