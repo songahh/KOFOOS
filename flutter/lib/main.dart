4ba37e577b2824912595e89d032b0e57ab9587ce
권정훈 <dhtmxk8134@naver.com>
2024-02-02 15:55:48 +0900

    chore: [develop-front] 타이틀 변경

--- flutter/lib/main.dart
+++ flutter/lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'KOFOOS',
+      title: 'KOFO',
       initialBinding: BindingsBuilder(() {
         Get.put(RootController());
       }),
