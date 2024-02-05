39e0f2c81203bcb3651b938870ccdd0aaa4cb42f
김라연 <fkdusrh@naver.com>
2024-02-05 10:42:37 +0900

    feat: [feature/wishlist-back] wishlist 조회 수정

---
+++ server/src/main/java/com/kofoos/api/wishlist/dto/FolderDto.java
@@ -0,0 +1,25 @@
+package com.kofoos.api.wishlist.dto;
+
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.databind.ser.std.ToStringSerializer;
+import lombok.Data;
+
+import java.util.List;
+
+
+@Data
+@JsonSerialize(using = ToStringSerializer.class)
+public class FolderDto {
+    private int folderId;
+    private String folderName;
+    private List<WishlistDto> items;
+
+    public FolderDto(int folderId, String folderName) {
+        this.folderId = folderId;
+        this.folderName = folderName;
+    }
+
+    public void setItems(List<WishlistDto> items) {
+        this.items = items;
+    }
+}
