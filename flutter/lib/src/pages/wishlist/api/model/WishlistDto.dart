d267fbadcb1436fa57d9d7c06a388ab0fab1dac1
권정훈 <dhtmxk8134@naver.com>
2024-02-07 18:05:13 +0900

    feat: [develop-front] 플러터 랭킹 수정
    Former-commit-id: 28e11b0816362ac8d617bc2080086e0886efee1c

--- flutter/lib/src/pages/wishlist/api/model/WishlistDto.dart
+++ flutter/lib/src/pages/wishlist/api/model/WishlistDto.dart
@@ -3,22 +3,22 @@ import 'dart:convert';
 class WishlistDto {
   final int wishlistItemId;
   final int bought;
-  final int productId;
+  final String itemNo;
   final String imageUrl;
 
-  WishlistDto({required this.wishlistItemId ,required this.bought, required this.productId, required this.imageUrl});
+  WishlistDto({required this.wishlistItemId ,required this.bought, required this.itemNo, required this.imageUrl});
 
   factory WishlistDto.fromJson(Map<String, dynamic> json) {
     return WishlistDto(
       wishlistItemId: json['wishlistItemId'],
       bought: json['bought'],
-      productId: json['productId'],
+      itemNo: json['itemNo'],
       imageUrl: json['imageUrl'],
     );
   }
   @override
   String toString() {
-    return 'wishlistItemId $wishlistItemId,bought: $bought, productId: $productId, imageUrl: $imageUrl';
+    return 'wishlistItemId $wishlistItemId,bought: $bought, itemNo: $itemNo, imageUrl: $imageUrl';
   }
 
 }
