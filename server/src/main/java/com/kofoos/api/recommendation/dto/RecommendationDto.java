8189d2de0e1f5bf307d9d44eca88a176332fb384
songahh <gunsong2@gmail.com>
2024-02-08 14:24:50 +0900

    feat: [feature/recommendation-back] returnDto에 알러지 추가
    Former-commit-id: 2334dbe45409d847e0f87480a45dfb5d9e004d75

--- server/src/main/java/com/kofoos/api/recommendation/dto/RecommendationDto.java
+++ server/src/main/java/com/kofoos/api/recommendation/dto/RecommendationDto.java
@@ -1,21 +1,33 @@
 package com.kofoos.api.recommendation.dto;
 
+import com.kofoos.api.entity.DislikedMaterial;
 import com.kofoos.api.entity.Product;
+import com.kofoos.api.entity.ProductMaterial;
 import lombok.Builder;
 import lombok.Getter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Getter
 @Builder
 public class RecommendationDto {
 
-    private int productId;
+    private String itemNo;
     private String imgUrl;
+    private List<String> allergyInfo;
 
     public static RecommendationDto of(Product entity){
         return RecommendationDto.builder()
-                .productId(entity.getId())
+                .itemNo(entity.getItemNo())
                 .imgUrl((entity.getImage()==null)? null : entity.getImage().getImgUrl())
-                .build();
+                .allergyInfo((entity.getProductMaterials()==null)?
+                        new ArrayList<>() :
+                        entity.getProductMaterials().stream()
+                                .map(ProductMaterial::getDislikedMaterial)
+                                .map(DislikedMaterial::getName)
+                                .toList()
+                ).build();
     }
 
 }
