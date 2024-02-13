3fa6deecc994721e9544ae0e259d7b972e8312e3
김도형 <ehgud619@naver.com>
2024-02-13 13:37:47 +0900

    Revert "Merge branch 'feature/dohyeong' into 'develop'"
    This reverts merge request !55
    
    Former-commit-id: 8baaaf6ae5b1d231f447cf02aea2cf5f7df160da

--- flutter/lib/src/pages/mypage/func/users_update_lang_func.dart
+++ flutter/lib/src/pages/mypage/func/users_update_lang_func.dart
@@ -1,16 +1,7 @@
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-import 'package:get/get.dart';
-import 'package:provider/provider.dart';
-import '../../../common/device_controller.dart';
-import '../api/mypage_api.dart';
-import '../mypage_notifier.dart';
 
 void usersUpdateLangFunc(BuildContext context) {
-  final DeviceController deviceController = Get.find<DeviceController>();
-  final MyPageApi myPageApi = MyPageApi(); // API 인스턴스 생성
-  final myPageNotifier = context.read<MyPageNotifier>();
-
   showDialog(
     context: context,
     builder: (BuildContext context) {
@@ -19,28 +10,22 @@ void usersUpdateLangFunc(BuildContext context) {
         children: [
           ListTile(
             title: Text('English'),
-            onTap: () async{
-              print('영어 선택');
-              await myPageApi.updateUserLanguage(deviceController.deviceId.value, 'en_US');
-              myPageNotifier.fetchMyPageInfo(deviceController.deviceId.value); // 언어 업데이트 후 정보 갱신
+            onTap: () {
+              print('영어 선택 로직 추가(유저 언어 업데이트)');
               Navigator.pop(context);
             },
           ),
           ListTile(
             title: Text('中文'),
-            onTap: () async{
-              print('중국어 선택');
-              await myPageApi.updateUserLanguage(deviceController.deviceId.value, 'zh_CN');
-              myPageNotifier.fetchMyPageInfo(deviceController.deviceId.value); // 언어 업데이트 후 정보 갱신
+            onTap: () {
+              print('중국어 선택 로직 추가(유저 언어 업데이트)');
               Navigator.pop(context);
             },
           ),
           ListTile(
             title: Text('日本語'),
-            onTap: () async{
-              print('일본어 선택');
-              await myPageApi.updateUserLanguage(deviceController.deviceId.value, 'ja_JP');
-              myPageNotifier.fetchMyPageInfo(deviceController.deviceId.value); // 언어 업데이트 후 정보 갱신
+            onTap: () {
+              print('일본어 선택 로직 추가(유저 언어 업데이트)');
               Navigator.pop(context);
             },
           ),
@@ -48,20 +33,4 @@ void usersUpdateLangFunc(BuildContext context) {
       );
     },
   );
-}
-
-//언어 코드와 언어 이름 매핑
-String getLanguageDisplayName(String languageCode) {
-  switch (languageCode) {
-    case 'ko_KR':
-      return '한국어';
-    case 'en_US':
-      return 'English';
-    case 'zh_CN':
-      return '中文';
-    case 'ja_JP':
-      return '日本語';
-    default:
-      return 'Unknown';
-  }
 }
