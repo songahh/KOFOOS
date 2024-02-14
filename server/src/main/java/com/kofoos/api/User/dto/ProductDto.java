6e15f28b3cce5f8ce737aaa1e0ee447e758120ec
현민수 <gusalstn91@ajou.ac.kr>
2024-02-14 10:27:13 +0900

    fix: [feature/history] 정렬 변경
    Former-commit-id: 3d0f95760231d2a8ad04ce6ac0eaee5cc5755767

--- server/src/main/java/com/kofoos/api/User/dto/ProductDto.java
+++ server/src/main/java/com/kofoos/api/User/dto/ProductDto.java
@@ -2,11 +2,13 @@ package com.kofoos.api.User.dto;
 
 import lombok.Builder;
 import lombok.Data;
+import lombok.EqualsAndHashCode;
 
 import java.time.LocalDateTime;
 
 @Data
 @Builder
+@EqualsAndHashCode(of = {"productUrl", "productItemNo"})
 public class ProductDto implements Comparable<ProductDto>{
     private String productUrl; // 이미지 URL
     private String productItemNo; // 제품 Item No
@@ -14,8 +16,9 @@ public class ProductDto implements Comparable<ProductDto>{
 
     @Override
     public int compareTo(ProductDto o) {
-        return this.date.compareTo(o.date);
+        return o.date.compareTo(this.date);
     }
+
 
     // 생성자, 게터, 세터 등은 Lombok @Data와 @Builder가 자동으로 처리
 }
