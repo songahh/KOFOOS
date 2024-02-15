68d2a6d43dc0f0895f97a77ccda90d7467222775
김도형 <ehgud619@naver.com>
2024-02-15 22:24:08 +0900

    fix:[develop]디버그 배너 제거 Former-commit-id: cfa1d456f2f6fe5d9aa3202a5b65b7a8279f7b31

--- flutter/lib/src/pages/register/register.dart
+++ flutter/lib/src/pages/register/register.dart
@@ -10,6 +10,7 @@ class StartApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       title: 'KOFOOS',
       theme: ThemeData(
         primarySwatch: Colors.blue,
