3218813843ff43af9e7efc6e78f58e3136cb16fb
김라연 <fkdusrh@naver.com>
2024-02-15 21:46:01 +0900

    feat: [feature/wishlist-front] 위시리스트 진단 결과 저장 기능 구현
    Former-commit-id: 414a0554f031714d60bf0d0c8d00528c4742f00b

--- flutter/lib/src/pages/wishlist/WishlistDetectionDto.dart
+++ flutter/lib/src/pages/wishlist/WishlistDetectionDto.dart
@@ -1,12 +1,13 @@
 class WishlistDetectionDto {
-  final String? itemNo;
-  final String imageUrl;
+   String? itemNo;
+   double score;
+   String imageUrl;
 
-  WishlistDetectionDto({required this.itemNo, required this.imageUrl});
+  WishlistDetectionDto({required this.itemNo, required this.score,required this.imageUrl});
 
   @override
   String toString() {
-    return 'itemNo: $itemNo, imageUrl: $imageUrl';
+    return 'itemNo: $itemNo, imageUrl: $imageUrl, score: $score';
   }
 
 
