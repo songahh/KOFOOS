c85809269c1c5cca9646933c55136562e28acb13
현민수 <gusalstn91@ajou.ac.kr>
2024-02-16 12:49:28 +0900

    젠킨스 문서 변경
    Former-commit-id: 66aa9901bb4eaba48f2869cf2669bdeed08515ba

--- server/src/main/java/com/kofoos/api/history/controller/HistoryController.java
+++ server/src/main/java/com/kofoos/api/history/controller/HistoryController.java
@@ -75,7 +75,7 @@ public class HistoryController {
 
     }
 
-    @Scheduled(fixedDelay = 30000000)
+    @Scheduled(fixedDelay = 300000)
     @Transactional
     public void updateSql(){
         Map<String, List<HistoryProductDto>> redisHistories = redisService.getAllRedisHistories();
